093188892b4d6a1365f631c9af6bedc999e6b0a6
MD ABDUL HADY <hady.onnorokom@gmail.com>
2022-05-12 10:46:06 +0600

    Clone usage statistics

---
+++ app/src/main/java/at/ciit/usagestats/MainActivity.java
@@ -0,0 +1,302 @@
+package at.ciit.usagestats;
+/*
+  Created by Javatraining www.ciit.at (Amjad) on 16,August,2020
+*/
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AppOpsManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.icu.util.Calendar;
+import android.net.IpSecManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button enableBtn, showBtn;
+    TextView permissionDescriptionTv, usageTv;
+    ListView appsList;
+
+    private Button dailyBtn, sevenDaysBtn, monthBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enableBtn = findViewById(R.id.enable_btn);
+        showBtn = findViewById(R.id.show_btn);
+        permissionDescriptionTv = findViewById(R.id.permission_description_tv);
+        usageTv = findViewById(R.id.usage_tv);
+        appsList = findViewById(R.id.apps_list);
+        dailyBtn = findViewById(R.id.dailyBtn);
+        sevenDaysBtn = findViewById(R.id.sevenDaysBtn);
+        monthBtn = findViewById(R.id.monthBtn);
+
+        dailyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadStatistics(4, startTime(4), endTime(4));
+            }
+        });
+
+        sevenDaysBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadStatistics(1, startTime(1), endTime(1));
+            }
+        });
+
+        monthBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadStatistics(2, startTime(2), endTime(2));
+            }
+        });
+
+        loadStatistics(4, startTime(4), endTime(4));
+    }
+
+
+    // each time the application gets in foreground -> getGrantStatus and render the corresponding buttons
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (getGrantStatus()) {
+            showHideWithPermission();
+            showBtn.setOnClickListener(view -> loadStatistics(1, startTime(4), endTime(4)));
+        } else {
+            showHideNoPermission();
+            enableBtn.setOnClickListener(view -> startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)));
+        }
+    }
+
+    public long startTime(int type) {
+        Calendar calendar = Calendar.getInstance();
+        if (type == 4) {
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+        }
+        if (type == 1) {
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.add(Calendar.DATE, -7);
+        }
+        if (type == 2) {
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.add(Calendar.DATE, -30);
+        }
+        return calendar.getTimeInMillis();
+    }
+
+    public long endTime(int type) {
+        if (type == 4) {
+            return startTime(type) + 86400 * 1000;
+        }
+        if (type == 1) {
+            return startTime(type) + (86400 * 7 * 1000);
+        }
+        if (type == 2) {
+            return startTime(type) + (86400L * 30 * 1000);
+        }
+        return 0L;
+    }
+
+    /**
+     * load the usage stats for last 24h
+     */
+    public void loadStatistics(int type, long beginTime, long endTime) {
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.HOUR_OF_DAY, 0);
+//        calendar.set(Calendar.MINUTE, 0);
+//        calendar.set(Calendar.SECOND, 0);
+//        calendar.add(Calendar.DATE, -7);
+//        long beginTime = calendar.getTimeInMillis();
+//        long endTime = beginTime + 86400 * 1000;
+        //cal.add(Calendar.DAY_OF_YEAR, -24);
+        //Toast.makeText(this, ""+Calendar.DAY_OF_YEAR, Toast.LENGTH_SHORT).show();
+        UsageStatsManager usm = (UsageStatsManager) this.getSystemService(USAGE_STATS_SERVICE);
+        List<UsageStats> appList = usm.queryUsageStats(type,
+                beginTime, endTime);
+        appList = appList.stream().filter(app -> app.getTotalTimeInForeground() > 0).collect(Collectors.toList());
+
+        // Group the usageStats by application and sort them by total time in foreground
+        if (appList.size() > 0) {
+            Map<String, UsageStats> mySortedMap = new TreeMap<>();
+            for (UsageStats usageStats : appList) {
+                mySortedMap.put(usageStats.getPackageName(), usageStats);
+            }
+            showAppsUsage(mySortedMap);
+        }
+    }
+
+
+    public void showAppsUsage(Map<String, UsageStats> mySortedMap) {
+        //public void showAppsUsage(List<UsageStats> usageStatsList) {
+        ArrayList<App> appsList = new ArrayList<>();
+        List<UsageStats> usageStatsList = new ArrayList<>(mySortedMap.values());
+
+        // sort the applications by time spent in foreground
+        Collections.sort(usageStatsList, (z1, z2) -> Long.compare(z1.getTotalTimeInForeground(), z2.getTotalTimeInForeground()));
+
+        // get total time of apps usage to calculate the usagePercentage for each app
+        long totalTime = usageStatsList.stream().map(UsageStats::getTotalTimeInForeground).mapToLong(Long::longValue).sum();
+
+        //fill the appsList
+        for (UsageStats usageStats : usageStatsList) {
+            try {
+                String packageName = usageStats.getPackageName();
+                Drawable icon = getDrawable(R.drawable.no_image);
+                String[] packageNames = packageName.split("\\.");
+                String appName = packageNames[packageNames.length - 1].trim();
+
+
+                if (isAppInfoAvailable(usageStats)) {
+                    ApplicationInfo ai = getApplicationContext().getPackageManager().getApplicationInfo(packageName, 0);
+                    icon = getApplicationContext().getPackageManager().getApplicationIcon(ai);
+                    appName = getApplicationContext().getPackageManager().getApplicationLabel(ai).toString();
+                }
+
+                String usageDuration = getDurationBreakdown(usageStats.getTotalTimeInForeground());
+                int usagePercentage = (int) (usageStats.getTotalTimeInForeground() * 100 / totalTime);
+
+                App usageStatDTO = new App(icon, appName, usagePercentage, usageDuration);
+                appsList.add(usageStatDTO);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        // reverse the list to get most usage first
+        Collections.reverse(appsList);
+        // build the adapter
+        AppsAdapter adapter = new AppsAdapter(this, appsList);
+
+        // attach the adapter to a ListView
+        ListView listView = findViewById(R.id.apps_list);
+        listView.setAdapter(adapter);
+
+        showHideItemsWhenShowApps();
+    }
+
+    /**
+     * check if PACKAGE_USAGE_STATS permission is aloowed for this application
+     *
+     * @return true if permission granted
+     */
+    private boolean getGrantStatus() {
+        AppOpsManager appOps = (AppOpsManager) getApplicationContext()
+                .getSystemService(Context.APP_OPS_SERVICE);
+
+        int mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), getApplicationContext().getPackageName());
+
+        if (mode == AppOpsManager.MODE_DEFAULT) {
+            return (getApplicationContext().checkCallingOrSelfPermission(android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED);
+        } else {
+            return (mode == MODE_ALLOWED);
+        }
+    }
+
+    /**
+     * check if the application info is still existing in the device / otherwise it's not possible to show app detail
+     *
+     * @return true if application info is available
+     */
+    private boolean isAppInfoAvailable(UsageStats usageStats) {
+        try {
+            getApplicationContext().getPackageManager().getApplicationInfo(usageStats.getPackageName(), 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+
+    /**
+     * helper method to get string in format hh:mm:ss from miliseconds
+     *
+     * @param millis (application time in foreground)
+     * @return string in format hh:mm:ss from miliseconds
+     */
+    private String getDurationBreakdown(long millis) {
+        if (millis < 0) {
+            throw new IllegalArgumentException("Duration must be greater than zero!");
+        }
+
+        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+        millis -= TimeUnit.HOURS.toMillis(hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        millis -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+        return (hours + " h " + minutes + " m " + seconds + " s");
+    }
+
+
+    /**
+     * helper method used to show/hide items in the view when  PACKAGE_USAGE_STATS permission is not allowed
+     */
+    public void showHideNoPermission() {
+        enableBtn.setVisibility(View.VISIBLE);
+        permissionDescriptionTv.setVisibility(View.VISIBLE);
+        showBtn.setVisibility(View.GONE);
+        usageTv.setVisibility(View.GONE);
+        appsList.setVisibility(View.GONE);
+
+    }
+
+    /**
+     * helper method used to show/hide items in the view when  PACKAGE_USAGE_STATS permission allowed
+     */
+    public void showHideWithPermission() {
+        enableBtn.setVisibility(View.GONE);
+        permissionDescriptionTv.setVisibility(View.GONE);
+        showBtn.setVisibility(View.VISIBLE);
+        usageTv.setVisibility(View.GONE);
+        appsList.setVisibility(View.GONE);
+    }
+
+    /**
+     * helper method used to show/hide items in the view when showing the apps list
+     */
+    public void showHideItemsWhenShowApps() {
+        enableBtn.setVisibility(View.GONE);
+        permissionDescriptionTv.setVisibility(View.GONE);
+        showBtn.setVisibility(View.GONE);
+        usageTv.setVisibility(View.VISIBLE);
+        appsList.setVisibility(View.VISIBLE);
+
+    }
+}
